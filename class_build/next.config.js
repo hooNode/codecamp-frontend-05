34668f5aecc24c09ba006fb4223e963eef5949bd
kth7024 <guinness5167@gmail.com>
2022-03-07 13:19:48 +0900

    git 충돌해제

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  //스태틱 페이지 path 설정해주기
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
